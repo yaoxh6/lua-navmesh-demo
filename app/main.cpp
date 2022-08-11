d59ffa40948e759fdb3d962b7595afd5527c475e
yaoxh6 <2879813862@qq.com>
2022-08-11 21:03:54 +0800

    fix

--- app/main.cpp
+++ app/main.cpp
@@ -2,15 +2,11 @@
 #include <cassert>
 #include "lua_navmesh/lua_navmesh_export.h"
 
-extern "C" int luaopen_luna(lua_State * L);
-
-
 int main(int argc, char** argv)
 {
 	lua_State* m_L = luaL_newstate();
 	if (!m_L)
 		return false;
-	luaL_requiref(m_L, "luna", luaopen_luna, 0);
 	luaL_requiref(m_L, "navmesh", luaopen_navmesh, 0);
 	luaL_openlibs(m_L);
 
--- lua_navmesh/recast_navmesh.cpp
+++ lua_navmesh/recast_navmesh.cpp
@@ -1,5 +1,5 @@
 #include "recast_navmesh.h"
-#include "lua_log.h"
+//#include "lua_log.h"
 
 const FVector FVector::ZeroVector(0.0f, 0.0f, 0.0f);
 FVector::FVector(dtReal InX, dtReal InY, dtReal InZ)
@@ -49,14 +49,14 @@ bool recast_navmesh::LoadNavMeshFile(const char* InNavDataPath)
     if (!NavMesh) {
         NavMesh = dtAllocNavMesh();
         if (!NavMesh) {
-            LUA_LOG_ERR("recast_navmesh::LoadNavMeshFile dtAllocNavMesh failed\n");
+            // LUA_LOG_ERR("recast_navmesh::LoadNavMeshFile dtAllocNavMesh failed\n");
             return false;
         }
     }
 
     FILE* pd = fopen(InNavDataPath, "rb");
     if (!pd) {
-        LUA_LOG_ERR("recast_navmesh::LoadNavMeshFile open file %s failed\n", InNavDataPath);
+        // LUA_LOG_ERR("recast_navmesh::LoadNavMeshFile open file %s failed\n", InNavDataPath);
         return false;
     }
 
@@ -70,40 +70,40 @@ bool recast_navmesh::LoadNavMeshFileInner(dtNavMesh* NavMesh, FILE* pd)
     NavMeshSetHeader Header;
     auto count = fread((uint8_t*)(&Header), sizeof(Header), 1, pd);
     if (count != 1) {
-        LUA_LOG_ERR("recast_navmesh::LoadNavMeshFileInner read NavMeshSetHeader failed\n");
+        // LUA_LOG_ERR("recast_navmesh::LoadNavMeshFileInner read NavMeshSetHeader failed\n");
         return false;
     }
 
     if (Header.magic != NavmeshSetMagic || Header.version != NavmeshSetVersion || Header.numTiles <= 0) {
-        LUA_LOG_ERR("recast_navmesh::LoadNavMeshFileInner NavMeshSetHeader check failed\n");
+        // LUA_LOG_ERR("recast_navmesh::LoadNavMeshFileInner NavMeshSetHeader check failed\n");
         return false;
     }
 
     dtStatus Status = NavMesh->init(&Header.params);
     if (!dtStatusSucceed(Status)) {
-        LUA_LOG_ERR("recast_navmesh::LoadNavMeshFileInner NavMesh init failed\n");
+        // LUA_LOG_ERR("recast_navmesh::LoadNavMeshFileInner NavMesh init failed\n");
         return false;
     }
     for (int i = 0; i < Header.numTiles; ++i) {
         NavMeshTileHeader TileHeader;
         auto count = fread((uint8_t*)(&TileHeader), sizeof(TileHeader), 1, pd);
         if (count != 1 || TileHeader.dataSize <= 0 || TileHeader.dataSize > MaxDataSize) {
-            LUA_LOG_ERR("recast_navmesh::LoadNavMeshFileInner NavMeshTileHeader check failed\n");
+            // LUA_LOG_ERR("recast_navmesh::LoadNavMeshFileInner NavMeshTileHeader check failed\n");
             return false;
         }
         uint8_t* data = reinterpret_cast<uint8_t*>(dtAlloc(TileHeader.dataSize, DT_ALLOC_PERM_NAVMESH));
         if (!data) {
-            LUA_LOG_ERR("recast_navmesh::LoadNavMeshFileInner NavMeshTileHeader dtAlloc failed\n");
+            // LUA_LOG_ERR("recast_navmesh::LoadNavMeshFileInner NavMeshTileHeader dtAlloc failed\n");
             return false;
         }
         count = fread(data, TileHeader.dataSize, 1, pd);
         if (count != 1) {
-            LUA_LOG_ERR("recast_navmesh::LoadNavMeshFileInner NavMeshTileHeader read failed\n");
+            // LUA_LOG_ERR("recast_navmesh::LoadNavMeshFileInner NavMeshTileHeader read failed\n");
             return false;
         }
         Status = NavMesh->addTile(data, TileHeader.dataSize, DT_TILE_FREE_DATA, TileHeader.tileRef, nullptr);
         if (!dtStatusSucceed(Status)) {
-            LUA_LOG_ERR("recast_navmesh::LoadNavMeshFileInner addTile failed\n");
+            // LUA_LOG_ERR("recast_navmesh::LoadNavMeshFileInner addTile failed\n");
             return false;
         }
     }
@@ -128,12 +128,12 @@ bool recast_navmesh::InitNavQuery(uint16_t includeFlag, uint16_t excludeFlag, FV
     }
     NavQuery = dtAllocNavMeshQuery();
     if (!NavQuery) {
-        LUA_LOG_ERR("recast_navmesh::InitNavQuery NavQuery dtAllocNavMeshQuery failed\n");
+        // LUA_LOG_ERR("recast_navmesh::InitNavQuery NavQuery dtAllocNavMeshQuery failed\n");
         return false;
     }
     dtStatus initStatus = NavQuery->init(NavMesh, MAX_NAV_SEARCH_NODES);
     if (!dtStatusSucceed(initStatus)) {
-        LUA_LOG_ERR("recast_navmesh::InitNavQuery NavQuery init failed\n");
+        // LUA_LOG_ERR("recast_navmesh::InitNavQuery NavQuery init failed\n");
         return false;
     }
     return true;
@@ -149,20 +149,20 @@ bool recast_navmesh::GetPathRef(FVector& UnrealStart, FVector& UnrealEnd, dtStat
 
     NavQuery->findNearestPoly(&RecastStartPosToProject.x, Extent, &queryFilter, &StartPoly, &RecastStartPos.x);
     if (StartPoly == INVALID_NAVNODEREF) {
-        LUA_LOG_ERR("recast_navmesh::GetPathRef findNearestPoly x=%f, y=%f, z=%f, failed\n", UnrealStart.x, UnrealStart.y, UnrealStart.z);
+        // LUA_LOG_ERR("recast_navmesh::GetPathRef findNearestPoly x=%f, y=%f, z=%f, failed\n", UnrealStart.x, UnrealStart.y, UnrealStart.z);
         return false;
     }
 
     EndPoly = INVALID_NAVNODEREF;
     NavQuery->findNearestPoly(&RecastEndPosToProject.x, Extent, &queryFilter, &EndPoly, &RecastEndPos.x);
     if (EndPoly == INVALID_NAVNODEREF) {
-        LUA_LOG_ERR("recast_navmesh::GetPathRef findNearestPoly x=%f, y=%f, z=%f, failed\n", UnrealEnd.x, UnrealEnd.y, UnrealEnd.z);
+        // LUA_LOG_ERR("recast_navmesh::GetPathRef findNearestPoly x=%f, y=%f, z=%f, failed\n", UnrealEnd.x, UnrealEnd.y, UnrealEnd.z);
         return false;
     }
 
     FindPathStatus = NavQuery->findPath(StartPoly, EndPoly, &RecastStartPos.x, &RecastEndPos.x, FLT_MAX, &queryFilter, PathResult, 0);
     if (!dtStatusSucceed(FindPathStatus)) {
-        LUA_LOG_ERR("recast_navmesh::GetPathRef findPath failed\n");
+        // LUA_LOG_ERR("recast_navmesh::GetPathRef findPath failed\n");
         return false;
     }
     return true;
@@ -194,7 +194,7 @@ bool recast_navmesh::GetPathPoint(dtStatus& FindPathStatus, dtQueryResult& PathR
     PathPoints.clear();
     if (!dtStatusSucceed(StringPullStatus))
     {
-        LUA_LOG_ERR("recast_navmesh::GetPathPoint NavQuery findStraightPath failed\n");
+        // LUA_LOG_ERR("recast_navmesh::GetPathPoint NavQuery findStraightPath failed\n");
         return false;
     }
     PathPoints.resize(StringPullResult.size());
